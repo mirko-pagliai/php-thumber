a90428b0315f2a87e4f064108475646064424b5e
mirko-pagliai <mirko.pagliai@gmail.com>
2019-08-28 14:54:46 +0200

    added some exceptions

---
+++ src/Exception/NotReadableImageException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of php-thumber.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/php-thumber
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace PhpThumber\Exception;
+
+use RuntimeException;
+
+class NotReadableImageException extends RuntimeException
+{
+
+}
